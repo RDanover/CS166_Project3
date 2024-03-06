41ad7eff60aae01610b8788a0d450fda5c6d3a11
Rachel Danover <60625627+RDanover@users.noreply.github.com>
2024-03-05 19:22:44 -0800

    Update load_data.sql

--- cs166_project_phase_3_updated/sql/src/load_data.sql
+++ cs166_project_phase_3_updated/sql/src/load_data.sql
@@ -1,32 +1,32 @@
 COPY Users
-FROM '/home/csmajs/rdano001/CS166_Project3/cs166_project_phase_3_updated/data/users.csv'
+FROM 'users.csv'
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE users_userID_seq RESTART 101;
 
 COPY Store
-FROM '/home/csmajs/rdano001/CS166_Project3/cs166_project_phase_3_updated/data/stores.csv'
+FROM 'stores.csv'
 WITH DELIMITER ',' CSV HEADER;
 
 COPY Product
-FROM '/home/csmajs/rdano001/CS166_Project3/cs166_project_phase_3_updated/data/products.csv'
+FROM 'products.csv'
 WITH DELIMITER ',' CSV HEADER;
 
 COPY Warehouse
-FROM '/home/csmajs/rdano001/CS166_Project3/cs166_project_phase_3_updated/data/warehouse.csv'
+FROM 'warehouse.csv'
 WITH DELIMITER ',' CSV HEADER;
 
 COPY Orders
-FROM '/home/csmajs/rdano001/CS166_Project3/cs166_project_phase_3_updated/data/orders.csv'
+FROM 'orders.csv'
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE orders_orderNumber_seq RESTART 501;
 
 
 COPY ProductSupplyRequests
-FROM '/home/csmajs/rdano001/CS166_Project3/cs166_project_phase_3_updated/data/productSupplyRequests.csv'
+FROM 'productSupplyRequests.csv'
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE productsupplyrequests_requestNumber_seq RESTART 11;
 
 COPY ProductUpdates
-FROM '/home/csmajs/rdano001/CS166_Project3/cs166_project_phase_3_updated/data/productUpdates.csv'
+FROM 'productUpdates.csv'
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE productupdates_updateNumber_seq RESTART 51;
